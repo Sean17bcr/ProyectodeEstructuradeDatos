7268f1e746baa9fb7d0308f8503727497d8ad7cb
AleJPacas <91388448+AleJPacas@users.noreply.github.com>
2022-08-14 00:59:03 -0500

    Catalogós, modulo 2

---
+++ ProyectoED/src/main/java/com/mycompany/proyectoed/Modulo2_Catalog.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectoed;
+
+/**
+ *
+ * @author Sebastian Pacas
+ */
+public class Modulo2_Catalog {
+    
+}
